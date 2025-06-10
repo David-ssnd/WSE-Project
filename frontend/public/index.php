4e2de23565405884e47c3d5d1fa98d0d20dc35e8
ZetrixSK <zetrixgota@gmail.com>
2025-06-10 16:56:38 +0200

    add AccountController and account page with recipe management features

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -5,12 +5,14 @@ require __DIR__ . '/../vendor/autoload.php';
 use App\Router\Router;
 use App\Controller\HomeController;
 use App\Controller\RecipeController;
+use App\Controller\AccountController;
 
 $router = new Router();
 
 //routes
 $router->get('/', HomeController::class, 'index');
 $router->get('/recipe-page/', RecipeController::class, 'index');
+$router->get('/account/', AccountController::class, 'index');
 
 
 $router->dispatch();
