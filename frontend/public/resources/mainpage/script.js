86e7e5ee723ea65b9d1fe3aa1fb76a70b0029840
ZetrixSK <zetrixgota@gmail.com>
2025-06-10 17:17:56 +0200

    added fetch request

--- frontend/public/resources/mainpage/script.js
+++ frontend/public/resources/mainpage/script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // Fetching recipes
-    fetch('/resources/mainpage/recipes.json')
+    fetch('http://127.0.0.1:8081/api/recipes')
         .then(response => {
             if (!response.ok) {
             throw new Error('Network response was not ok ' + response);
